8ff07253d44ac72a0ceddbd88b4fbd9ef4c8eb7a
鸟 <yangyunh24@mails.tsinghua.edu.cn>
2026-05-15 23:10:36 +0800

    提交了test2.py和.vscode/settings.json文件。

---
+++ test2.py
@@ -0,0 +1,23 @@
+class ErNiao:
+    def __init__(self, name):
+        self.name = name
+
+    def find_son(self):
+        print(f"{self.name} 的好大儿是滑滑头！")
+
+class HuaTou(ErNiao):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def find_father(self):
+        print(f"{self.name} 的父亲是二鸟！")
+
+    def find_son(self):
+        print(f"{self.name} 的儿子是二鸟的孙子！")
+
+erniao = ErNiao("二鸟")
+huatou = HuaTou("滑滑头")
+
+erniao.find_son()
+huatou.find_father()
+huatou.find_son()
